d1a1d55bf01b0e340e33612100a159e53fab6a6f
Yurii Kazak <115292696+Mlx612@users.noreply.github.com>
2025-11-21 16:26:25 +0100

    Smiley now work

---
+++ CV9_test/Core/Src/main.c
@@ -0,0 +1,491 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usbd_hid.h"
+#include "math.h"
+#include <stdbool.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart3;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART3_UART_Init(void);
+/* USER CODE BEGIN PFP */
+void step(int dx, int dy, bool btn);
+void circle(int radius);
+static void wait_hid_ready(void);
+static void move_rel(int dx, int dy);
+static void arc(int radius, float start_deg, float end_deg);
+void smiley(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+extern USBD_HandleTypeDef hUsbDeviceFS;
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART3_UART_Init();
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN 2 */
+
+//  uint32_t i=0;
+  wait_hid_ready();
+//  circle(80);
+  smiley();
+//  arc(30, 200.0f, 340.0f);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+//	  HAL_Delay(2000);
+//	  step(0, 0, false);
+//	  while(i<1){
+//	  uint8_t buff[4];
+//	  buff[0] = 0x01; // stiskni leve tlacitko
+//	  buff[1] = (int8_t)(10); // posun X +10
+//	  buff[2] = (int8_t)(-3); // posun Y -3
+//	  buff[3] = 0; // bez scrollu
+//	  USBD_HID_SendReport(&hUsbDeviceFS, buff, sizeof(buff));
+//	  HAL_Delay(USBD_HID_GetPollingInterval(&hUsbDeviceFS));
+//	  i++;
+//	  circle(50);
+//	  }
+	  /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : USER_Btn_Pin */
+  GPIO_InitStruct.Pin = USER_Btn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RMII_MDC_Pin RMII_RXD0_Pin RMII_RXD1_Pin */
+  GPIO_InitStruct.Pin = RMII_MDC_Pin|RMII_RXD0_Pin|RMII_RXD1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RMII_REF_CLK_Pin RMII_MDIO_Pin RMII_CRS_DV_Pin */
+  GPIO_InitStruct.Pin = RMII_REF_CLK_Pin|RMII_MDIO_Pin|RMII_CRS_DV_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
+  GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : RMII_TXD1_Pin */
+  GPIO_InitStruct.Pin = RMII_TXD1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+  HAL_GPIO_Init(RMII_TXD1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : USB_PowerSwitchOn_Pin */
+  GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(USB_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : USB_OverCurrent_Pin */
+  GPIO_InitStruct.Pin = USB_OverCurrent_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USB_OverCurrent_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RMII_TX_EN_Pin RMII_TXD0_Pin */
+  GPIO_InitStruct.Pin = RMII_TX_EN_Pin|RMII_TXD0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void step(int dx, int dy, bool btn){
+    uint8_t buff[4];
+
+    buff[0] = btn ? 0x01 : 0x00;     // бит 0 = левая кнопка
+    buff[1] = (int8_t)dx;            // относительный сдвиг X
+    buff[2] = (int8_t)dy;            // относительный сдвиг Y
+    buff[3] = 0;                     // без скролла
+
+    USBD_HID_SendReport(&hUsbDeviceFS, buff, sizeof(buff));
+    HAL_Delay(USBD_HID_GetPollingInterval(&hUsbDeviceFS));
+}
+
+void circle(int radius){
+    const int steps = 80;                        // количество точек (можешь менять)
+    const float dphi = 2.0f * (float)M_PI / steps;
+
+    // целочисленные предыдущие координаты
+    int sx_prev = 0;
+    int sy_prev = 0;
+
+    // первую точку берём φ = 0
+    int sx0 = (int)lroundf(radius * cosf(0.0f));
+    int sy0 = (int)lroundf(radius * sinf(0.0f));
+    sx_prev = sx0;
+    sy_prev = sy0;
+
+    // нажимаем кнопку перед рисованием
+    step(0, 0, true);
+
+    for (int i = 1; i <= steps; i++)
+    {
+        float phi = i * dphi;
+
+        float x = radius * cosf(phi);
+        float y = radius * sinf(phi);
+
+        int sx = (int)lroundf(x);
+        int sy = (int)lroundf(y);
+
+        int dx = sx - sx_prev;
+        int dy = sy - sy_prev;
+
+        sx_prev = sx;
+        sy_prev = sy;
+
+        // страховка от выхода за диапазон int8_t
+        if (dx > 127)  dx = 127;
+        if (dx < -128) dx = -128;
+        if (dy > 127)  dy = 127;
+        if (dy < -128) dy = -128;
+
+        step(dx, dy, true);   // рисуем с нажатой ЛКМ
+    }
+
+    // отпускаем кнопку в конце
+    step(0, 0, false);
+    step((-radius/2)-3.1415927, 0, false);
+}
+
+static void move_rel(int dx, int dy){
+    step(dx, dy, false);   // просто перемещение, кнопка не нажата
+}
+
+static void wait_hid_ready(void){ //дебаг для того что бы необрезало начальные значения
+    uint8_t buff[4] = {0, 0, 0, 0};
+
+    // Ждём пока Windows сконфигурирует устройство
+    while (hUsbDeviceFS.dev_state != USBD_STATE_CONFIGURED)
+    {
+        HAL_Delay(1);
+    }
+
+    while (USBD_HID_SendReport(&hUsbDeviceFS, buff, sizeof(buff)) == USBD_BUSY)
+    {
+        HAL_Delay(1);   // минимальная пауза
+    }
+
+    // Дополнительный короткий прогрев: 2–3 стабильных отчёта
+    for (int i = 0; i < 3; i++)
+    {
+        USBD_HID_SendReport(&hUsbDeviceFS, buff, sizeof(buff));
+        HAL_Delay(1);
+    }
+}
+
+static void arc(int radius, float start_deg, float end_deg){
+    const int steps = 40;   // сколько сегментов; можно подстроить
+    float start = start_deg * (float)M_PI / 180.0f;
+    float end   = end_deg   * (float)M_PI / 180.0f;
+    float dphi  = (end - start) / steps;
+
+    // начинаем из центра – сначала прыгаем в первую точку дуги
+    float phi = start;
+    int sx_prev = (int)lroundf(radius * cosf(phi));
+    int sy_prev = (int)lroundf(radius * sinf(phi));
+
+    // прыжок из центра в первую точку и зажимаем кнопку
+    step(sx_prev, sy_prev, true);
+
+    for (int i = 1; i <= steps; i++)
+    {
+        phi = start + i * dphi;
+        int sx = (int)lroundf(radius * cosf(phi));
+        int sy = (int)lroundf(radius * sinf(phi));
+
+        int dx = sx - sx_prev;
+        int dy = sy - sy_prev;
+
+        sx_prev = sx;
+        sy_prev = sy;
+
+        step(dx, (-1)*dy, true);    // рисуем дугу с зажатой кнопкой
+    }
+    step(0, 0, false);
+    // возвращаемся обратно в центр и отпускаем кнопку
+    step(-sx_prev, 0, false);
+}
+
+
+void smiley(void){
+    const int R_HEAD      = 80;  // радиус головы
+    const int R_EYE       = 8;   // радиус глаза
+    const int EYE_OFF_X   = 20;  // сдвиг глаз по X от центра
+    const int EYE_OFF_Y   = 18;  // сдвиг глаз по Y (вверх)
+    const int NOSE_LEN    = 30;  // длина носа (прямая линия)
+    const int MOUTH_R     = 45;  // радиус улыбки
+    const int MOUTH_OFF_Y = 30;  // сдвиг центра рта вниз
+
+    // 1) Голова – большая окружность
+    circle(R_HEAD);
+
+    // 2) Левый глаз
+    move_rel(-EYE_OFF_X, -EYE_OFF_Y);   // к центру левого глаза
+    circle(R_EYE);
+    move_rel(+EYE_OFF_X+R_EYE/2, +EYE_OFF_Y);   // вернулись в центр лица
+
+    // 3) Правый глаз
+    move_rel(+EYE_OFF_X+R_EYE/2, -EYE_OFF_Y+R_EYE/2);   // к центру правого глаза
+    circle(R_EYE);
+    move_rel(-EYE_OFF_X-R_EYE/2, +EYE_OFF_Y);   // снова центр лица
+
+    // 4) Нос – просто вертикальная линия вниз
+    for (int i = 0; i < NOSE_LEN; i++)
+    {
+        step(0, +1, true);   // рисуем линию
+    }
+    step(0, 0, false);       // отпустили кнопку
+    // вернёмся назад, чтобы центр опять был примерно там же
+    move_rel(0, 0);
+
+    // 5) Улыбка – дуга
+    move_rel(R_HEAD/2+3.14159, +MOUTH_OFF_Y);         // центр рта ниже центра лица
+    arc(MOUTH_R, 200.0f, 340.0f);      // нижняя дуга
+    move_rel(0, -MOUTH_OFF_Y);         // вернули курсор в центр
+}
+
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
